e35c23af19fe35040f33461b785165b236381816
ChouguleVrushali <vrushalichougule2505.com>
2024-08-16 21:45:24 +0530

    first commit

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const server=http.createServer((req,res)=>{
     const path=url.parse(req.url,true);
     const method=req.method;
     const queryPara=path.query;
-    console.log(queryPara)
+    // console.log(queryPara)
     const pathname=path.pathname.split("/");
+    // console.log(pathname);
     
 /*1>create a new task*/
     //post /tasks
@@ -24,31 +25,42 @@ const server=http.createServer((req,res)=>{
             const Data=JSON.parse(data);
             Data.id=i++;
             tasks.push(Data);
-            console.log("tasks=",tasks);
+            // console.log("tasks=",tasks);
             res.writeHead(201,{'Content-Type':'application/json'});
             res.end(JSON.stringify({message:"Task added",data:Data}));
         });
     }
 
-    // { status: 'completed', priority: 'high' }
-
 /*2>get all task*/
     //GET /tasks
     if(method==="GET" && pathname.length===2 && pathname[1]==="tasks"){
+        let num=0,array=[];
         if(tasks.length!=0){
-            // tasks.forEach((para)=>{
-            //     for(let temp in para){
-            //         if(para[temp]===queryPara[temp]){
-            //             console.log(para);
-            //         }
-            //     }
-            // })
-            res.writeHead(200,{'Content-Type':'application/json'});
-            res.end(JSON.stringify({message:"Task api called",data:{tasks}}));
+            for(let abc in queryPara){
+                num++;
+                console.log("num=",num)
+            }
+            if(num==0){
+                res.writeHead(200,{'Content-Type':'application/json'});  
+                res.end(JSON.stringify({message:"Task api called",data:tasks}));
+            }
+            //logic for query parameter
+            else{
+                tasks.forEach((para)=>{
+                    for(let temp in para){
+                        if(para[temp]===queryPara[temp]){
+                            array.push(para);
+                        }
+                    }
+                })
+                console.log(array)
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:"Task api called",data:array}));
+            }
         }
         else{
             res.writeHead(200,{'Content-Type':'application/json'});
-            res.end(JSON.stringify({message:"tasks is empty",data:{}}));
+            res.end(JSON.stringify({message:"tasks is empty"}));
         }
     }
 
@@ -58,28 +70,19 @@ const server=http.createServer((req,res)=>{
     if(method==="GET" && pathname.length===3 && pathname[1]==="tasks"){
         const Id=+pathname[2];
         if(Id<=tasks.length){
-            res.writeHead(200,{'Content-Type':'application/json'});
-            res.end(JSON.stringify({message:"Task api called",data:tasks[Id-1]}));
+            tasks.forEach((ele)=>{
+                if(ele.id==pathname[2]){
+                    res.writeHead(200,{'Content-Type':'application/json'});
+                    res.end(JSON.stringify({message:"Task api called",data:ele}));
+                }
+            });
         }
         else{
             res.writeHead(404,{'Content-Type':'application/json'});
             res.end(JSON.stringify({message:"Task not found",data:{}}));
         }
     }
-
-    // {
-    //     title:"task1",
-    //     status:"complete",
-    //     priority:"high"
-    // }
-
-    // {
-    //     title:"task title",
-    //     description:"task description",
-    //     deuDate:"20/08/2024",
-    //     status:"pending",
-    //     priority:"medium"
-    // }
+   
 
 /*4>update the task*/
     //PUT /tasks/:id
@@ -87,7 +90,6 @@ const server=http.createServer((req,res)=>{
         let data="";
         req.on("data",(chunk)=>{
             data+=chunk;
-            console.log(data);
         })
         req.on("end",()=>{
             let ID=+pathname[2];
@@ -100,19 +102,69 @@ const server=http.createServer((req,res)=>{
                 res.writeHead(200,{'Content-Type':'application/json'});
                 res.end(JSON.stringify({message:"task updated",data:tasks[pathname[2]-1]}));
             }
+            else{
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:"task updated"}));
+            }
         })
     }
 
-// /*5>delete a task by id*/
-    //DELETE /tasks/:id
-//     if(method==="DELETE" && pathname.length===3 && pathname[1]==="tasks"){
-        
-//     }
     
+/*5>delete a task by id*/
+    // DELETE /tasks/:id
+    if(method==="DELETE" && pathname.length===3 && pathname[1]==="tasks"){
+        const ID=+pathname[2];
+         tasks.splice(pathname[2]-1,1);
+         if(ID<=tasks.length){
+            res.writeHead(200,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:"task deleted"}));
+        }
+        else{
+            res.writeHead(404,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:"task deleted"}));
+        }
+    }
+
+/*6>Task prioritization*/
+    //PATCH /tasks/:id/priority
+    if(method==="PATCH" && pathname.length===4 && pathname[1]==="tasks" && pathname[3]==="priority"){
+        let body="";
+        req.on("data",(chunk)=>{
+            body=body+chunk
+        })
+        req.on("end",()=>{
+            body=JSON.parse(body);
+            tasks[pathname[2]-1].priority= body.priority; 
+            res.writeHead(200,{'Content-Type':'application/json'});
+            res.end(JSON.stringify({message:"priority updated",data:body}));
+        })
+    }
+
+/*7>Assign task to user*/
+    //PATCH /tasks/:id/assign
+    if(method==="PATCH" && pathname.length===4 && pathname[1]==="tasks" && pathname[3]==="assign"){
+            let body="";
+            req.on("data",(chunk)=>{
+                body=body+chunk
+            })
+            req.on("end",()=>{
+                body=JSON.parse(body);
+                tasks[pathname[2]-1].assignTo= body.assignTo; 
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:"priority updated",data:body}));
+            })
+    }
+
+/*8>Unassign task */
+   //PATCH /tasks/:id/unassign
+   if(method==="PATCH" && pathname.length===4 && pathname[1]==="tasks" && pathname[3]==="unassign"){
+        tasks[pathname[2]-1].assignTo=null;
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({message:"priority updated",data: tasks[pathname[2]-1]}));
+   }
+  
 })
 server.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-
 
